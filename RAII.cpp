45e85bc9459679991d7fe4f22c4ebdbf5da09f64
shangjinlong <shangjinlong@unisound.com>
2019-11-25 14:07:53 +0800

    :construction: RAII

--- RAII.cpp
+++ RAII.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 using namespace std;
 
@@ -23,21 +24,36 @@ RAII::~RAII() {
 void RAII::mutexTest() {
     {
         std::lock_guard<std::mutex> locker(m_mutex);
-        cout<<"lock_guard "<<__FUNCTION__<<endl;
+        cout<<"lock_guard "<<__FUNCTION__<<__LINE__<<endl;
     }
 
     {
         std::lock_guard<std::mutex> locker(m_mutex);
-        cout<<"lock_guard "<<__FUNCTION__<<endl;
+        cout<<"lock_guard "<<__FUNCTION__<<__LINE__<<endl;
     }
 
-    std::unique_lock<std::mutex> locker(m_mutex);
-    cout<<"unique_lock "<<__FUNCTION__<<endl;
+    /* 初始状态不上锁 */
+    std::unique_lock<std::mutex> locker(m_mutex, std::defer_lock);
+    cout<<"unique_lock "<<__FUNCTION__<<__LINE__<<endl;
+    locker.lock(); /* 锁会随着析构自动解除 */
+    cout<<"unique_lock "<<__FUNCTION__<<__LINE__<<endl;
+}
+
+static void __raii_test(RAII* raii) {
+    if (NULL != raii) {
+        raii->mutexTest();
+    }
 }
 
 int main () {
     RAII raii;
-    raii.mutexTest();
+
+    std::thread t1(__raii_test, &raii);
+    std::thread t2(__raii_test, &raii);
+
+    t1.join();
+    t2.join();
+    cout<<"both thread destroy"<<endl;
 
     return 0;
 }
